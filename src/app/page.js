64f274c51489a3d6acc937c9f0fa131908401fc9
Himanshu Rawat <officialhimanshurawat@gmail.com>
2024-09-13 14:55:52 +0530

    favicon added

--- src/app/page.js
+++ src/app/page.js
@@ -17,22 +17,28 @@ export default function Home() {
 			<div className=" relative">
 				<SplineScene />
 				<div className="absolute w-full z-10 bottom-0 px-4 flex justify-center items-center">
-					<div className=" gap-4 p-8 rounded-full flex text-center w-fit bg-black/40 backdrop-filter backdrop-blur-sm bg-opacity-10 py-4 uppercase font-bold">
-						<Link href="https://github.com/himanrawat">
+					<div className=" gap-4 p-8 rounded-full flex text-center w-fit bg-white/40 backdrop-filter backdrop-blur-sm bg-opacity-10 py-4 uppercase font-bold">
+						<Link href="https://github.com/himanrawat" target="_blank">
 							<Image src={github} alt="github" className="w-6 h-6 " />
 						</Link>
-						<Link href="https://www.linkedin.com/in/rawat-himanshu/">
+						<Link
+							href="https://www.linkedin.com/in/rawat-himanshu/"
+							target="_blank"
+						>
 							<Image src={linkedin} alt="Linkedin" className="w-6 h-6 " />
 						</Link>
-						<Link href="https://www.xing.com/profile/Himanshu_Rawat026243">
+						<Link
+							href="https://www.xing.com/profile/Himanshu_Rawat026243"
+							target="_blank"
+						>
 							<Image src={xing} alt="xing" className="w-6 h-6 " />
 						</Link>
-						<Link href="https://www.behance.net/himanshu_rawat">
+						<Link href="https://www.behance.net/himanshu_rawat" target="_blank">
 							<Image src={behance} alt="behance" className="w-6 h-6 " />
 						</Link>
-						<Link href="https://www.xing.com/profile/Himanshu_Rawat026243">
+						<a href="mailto:officialhimanshurawat@gmail.com" target="_blank">
 							<Image src={at} alt="gmail" className="w-6 h-6 " />
-						</Link>
+						</a>
 					</div>
 				</div>
 			</div>
