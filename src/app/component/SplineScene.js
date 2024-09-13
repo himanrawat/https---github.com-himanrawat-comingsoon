1b5bf4376de25baeb8ace0eef0c04b1bc9d6936c
Himanshu Rawat <officialhimanshurawat@gmail.com>
2024-09-13 14:42:05 +0530

    initial commit

---
+++ src/app/component/SplineScene.js
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline/next";
+
+export default function Home() {
+	return (
+		<main className="w-[110vw] h-screen">
+			<Spline scene="https://prod.spline.design/yKsEpaFqI2VTzWzK/scene.splinecode" />
+		</main>
+	);
+}
